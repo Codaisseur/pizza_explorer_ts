9ace36d63eecd1fceaaf592d93204c237145260d
Kelley van Evert <hello@kelleyvanevert.nl>
2020-09-13 22:27:16 +0200

    Added Redux store, connected with React-Redux

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 1. Initialized a new React app with `create-react-app`
+2. Added Redux store, connected with React-Redux
---
+++ src/store/types.ts
@@ -0,0 +1,23 @@
+export type User = {
+  name: string;
+  email: string;
+};
+
+export type Pizza = {
+  id: number;
+  name: string;
+  description: string;
+  bought: number;
+};
+
+export type State = {
+  user: User;
+  pizzas: Pizza[];
+};
+
+export type Action =
+  | {
+      type: "add_pizza";
+      payload: Pizza;
+    }
+  | { type: "...more_to_come..." };
