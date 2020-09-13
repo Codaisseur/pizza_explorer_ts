6eb2d094c8b29ac161713c62b8b4eec2d4ec41c9
Kelley van Evert <hello@kelleyvanevert.nl>
2020-09-13 22:32:30 +0200

    Added a PizzaList component stub

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 1. Initialized a new React app with `create-react-app`
 2. Added Redux store, connected with React-Redux
 3. Added Redux devtools integration
+4. Added a PizzaList component stub
---
+++ src/components/PizzaList.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PizzaList() {
+  return (
+    <div>
+      <h1>Pizza Explorer</h1>
+      <p>TODO: the list of pizzas</p>
+    </div>
+  );
+}
